58702f9525c4d5978953948cef98cdb3965d784b
Alex Withington-Smith <alex@awithsmith.com>
2020-10-04 13:21:27 +0100

    added var to store gameboard and logic to display gameboard to players

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,8 +1,24 @@
 # OBJECTS
+# game board
 class Board
-  def initialize(row1, row2, row3)
-    @row1 = row1
-    @row2 = row2
-    @row3 = row3
+  def initialize
+    # var for each cell
+    # ' ' indicates blank
+    # else X or O
+    @spaces =
+      [[' ', ' ', ' '],
+       [' ', ' ', ' '],
+       [' ', ' ', ' ']]
   end
+
+  # puts each row of board
+  # to update players
+  def draw_board
+    @spaces.each do |row|
+      puts "| #{row[0]} | #{row[1]} | #{row[2]} |"
+    end
+  end  
 end
+
+test = Board.new
+test.draw_board
